1793bd584c0107c5872fd814b05428cdb5d0dd0e
‘Matt <matthewparisien4@gmail.com>
2021-06-17 16:36:24 +0000

    Testing for loops for fun

---
+++ test-for.js
@@ -0,0 +1,11 @@
+let names = ["Matt", "Caroline", "Denis", "Tessa", "Ollie", "Marie"]
+
+for (let i=0; i<=names.length; i++) {
+  if (names[i] === 'Caroline' || names[i] === "Tessa"){
+    console.log("my name is" + " " + names[i] + "!");
+  } 
+}
+
+
+
+
