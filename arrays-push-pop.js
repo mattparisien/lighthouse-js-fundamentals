27a20ea6ebfe218511a0eed81c5820390dad1218
‘Matt <matthewparisien4@gmail.com>
2021-06-18 12:12:02 +0000

    Testing push() and pop() methods in arrays

---
+++ arrays-push-pop.js
@@ -0,0 +1,11 @@
+var donuts = ["glazed", "strawberry frosted", "powdered", "Boston creme"];
+
+
+donuts.pop();
+donuts.pop();
+donuts.pop();
+donuts.push("maple walnut");
+donuts.pop();
+donuts.push("sprinkled");
+
+console.log(donuts);
