ff2dc38c8f4dd51ca421427decc55ccfc28c4f5c
‘Matt <matthewparisien4@gmail.com>
2021-06-25 19:11:00 +0000

    Checking to see if air samples match a given threshold

---
+++ katas/kata-seven.js
@@ -0,0 +1,29 @@
+const checkAir = function (samples, threshold) {
+  let dirtySamples = "";
+  for (i = 0; i < samples.length; i++) {
+    if (samples[i] === "dirty") {
+      dirtySamples++ 
+    }
+  }
+  dirtySamples = dirtySamples / samples.length
+  if (dirtySamples > threshold) {
+    return "Polluted"
+  } else {
+    return "Clean"
+  }
+}
+
+console.log(checkAir(
+  ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
+  0.3
+));
+
+console.log(checkAir(
+  ['dirty', 'dirty', 'dirty', 'dirty', 'clean'],
+  0.25
+));
+
+console.log(checkAir(
+  ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
+  0.9
+))
